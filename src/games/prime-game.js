dcd1962a966ae853c60d940eca61ed21e8022dc0
Kirill <kirill.kltvkin@yandex.ru>
2023-05-21 15:53:05 +0300

    added prime game

---
+++ src/games/prime-game.js
@@ -0,0 +1,27 @@
+import { gameEngine, randomNumber } from '../index.js';
+
+const rules = '"yes" if given number is prime. Otherwise answer "no".';
+
+// game logic
+const logic = () => {
+  const randomNumber1 = randomNumber(30) + 1;
+  let correctAnswer = '';
+  const checkArr = [];
+  const question = (`${randomNumber1} `);
+  for (let i = 1; i <= randomNumber1; i += 1) {
+    if (randomNumber1 % i === 0) {
+      checkArr.push(i);
+    }
+  }
+  if (checkArr.length === 2) {
+    correctAnswer = 'yes';
+  }
+  if (checkArr.length !== 2) {
+    correctAnswer = 'no';
+  }
+  return [question, correctAnswer];
+};
+
+const primeGame = () => gameEngine(logic, rules);
+
+export default primeGame;
