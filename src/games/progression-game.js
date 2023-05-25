e174443b422ed534135594ce5d3b7e1c3b90b981
Kirill <kirill.kltvkin@yandex.ru>
2023-05-25 19:19:24 +0300

    update progression logic

--- src/games/progression-game.js
+++ src/games/progression-game.js
@@ -3,52 +3,27 @@ import getRandomNumber from '../modules/random-number.js';
 
 const gameRule = 'What number is missing in the progression?';
 
-const getNumLogic = () => {
-  const arrLogic = [1, 2, 3];
-  const result = Math.floor(Math.random() * arrLogic.length);
-  return result;
-};
-
-const getProgression = (numLogic, startItem) => {
+const getProgression = (step, startItem) => {
   const arr = [];
   let firstItem = startItem;
-  switch (numLogic) {
-    // every step + 2
-    case 0:
-      for (let i = 0; i < 10; i += 1) {
-        firstItem += 2;
-        arr.push(firstItem);
-      }
-      break;
-    // every step +3
-    case 1:
-      for (let i = 0; i < 10; i += 1) {
-        firstItem += 3;
-        arr.push(firstItem);
-      }
-      break;
-    // every step +4
-    case 2:
-      for (let i = 0; i < 10; i += 1) {
-        firstItem += 4;
-        arr.push(firstItem);
-      }
-      break;
-    default:
-      console.log(`Error. numLogic = ${numLogic}`);
+  for (let i = 0; i < 10; i += 1) {
+    firstItem += step;
+    arr.push(firstItem);
   }
   return arr;
 };
 
 // game logic
 const getRoundData = () => {
-  const startProgression = 1;
-  const endProgression = 20;
+  const start = 1;
+  const startStep = 2;
+  const endProgressionNumbers = 20;
   const endMissedItem = 9;
-  const missedItem = getRandomNumber(startProgression, endMissedItem);
-  const startItem = getRandomNumber(startProgression, endProgression);
-  const numLogic = getNumLogic();
-  const progression = getProgression(numLogic, startItem);
+  const endStepNumbers = 4;
+  const missedItem = getRandomNumber(start, endMissedItem);
+  const startItem = getRandomNumber(start, endProgressionNumbers);
+  const step = getRandomNumber(startStep, endStepNumbers);
+  const progression = getProgression(step, startItem);
   const correctAnswer = String(progression[missedItem]);
   progression[missedItem] = '..';
   const question = progression.join(' ');
